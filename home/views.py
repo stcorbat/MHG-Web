cc9d316b45fe3b8c98928a28787dbf3a4ba0070d
stcorbat <corbatscott@gmail.com>
2020-02-24 11:50:37 -0500

    Created a base template Added a dummy index file and prototype login html files urls and views files are updated accordingly

--- home/views.py
+++ home/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html', context={})
+
+
+def login(request):
+    return render(request, 'login.html', context={})
